722f37efdc2aed49ff28f3662e6d6edab9297d75
Weirdman SMR <maga.saykhanov95@gmail.com>
2022-07-31 11:44:50 +0300

    Add comments

--- Dog_animate/script.js
+++ Dog_animate/script.js
@@ -1,23 +1,23 @@
-let playerState = 'idle'
+let playerState = 'idle' // базовое положение и переключение с помощью селектора html
 const dropdown = document.getElementById('animations')
 dropdown.addEventListener('change', function(e) {
     playerState = e.target.value
 })
 
-const canvas = document.getElementById('canvas1')
+const canvas = document.getElementById('canvas1') // работа с canvas
 const ctx = canvas.getContext('2d')
 const CANVAS_WIDTH = canvas.width = 600
 const CANVAS_HEIGHT = canvas.height = 600
 
-const playerImage = new Image()
+const playerImage = new Image() // изображение
 playerImage.src = 'shadow_dog.png'
-const spriteWidth = 575
-const spriteHeight = 523
+const spriteWidth = 575 // ширина / максимальное количество фреймов (12)
+const spriteHeight = 523 // высота / количество положений (10)
 
-let gameFrame = 0
-const staggerFrames = 6
+let gameFrame = 0 // позволяет анимации... анимироваться :)
+const staggerFrames = 6 // фрейм - чем ниже, тем быстрее анимация
 const spriteAnimations = []
-const animationStates = [
+const animationStates = [ //количество фреймов каждой анимаций
     {
         name: 'idle',
         frames: 7
@@ -61,27 +61,34 @@ const animationStates = [
 ]
 
 animationStates.forEach((state, index) => {
+    /* нужно для того, чтобы в position в 80 строке 
+       можно было динамически менять значение под
+       количество спрайтов и не было нехватки анимации или бликов */
     let frames = {
         loc: [],
     }
     for (let j = 0; j < state.frames; j++) {
-        let PositionX = j * spriteWidth
-        let PositionY = index * spriteHeight
-        frames.loc.push({x: PositionX, y: PositionY})
+        let PositionX = j * spriteWidth // выбор спрайта по оси X
+        let PositionY = index * spriteHeight // выбор спрайта по оси Y
+        frames.loc.push({
+            x: PositionX,
+            y: PositionY
+        })
         
     }
-    spriteAnimations[state.name] = frames
+    spriteAnimations[state.name] = frames // передаём координаты для нужного спрайта
 })
 
 function animate() {
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT) // заготовка места для спрайта
     let position = Math.floor(gameFrame / staggerFrames) % spriteAnimations[playerState].loc.length
-    let frameX = spriteWidth * position
-    let frameY = spriteAnimations[playerState].loc[position].y
+    // сама переменная, где и анимируется изображение посредством быстрой смены позиции
+    let frameX = spriteWidth * position // начало обрезки изображения по оси Х
+    let frameY = spriteAnimations[playerState].loc[position].y // и оси Y
     
     ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
-
+    // вырисовка нужного спрайта
     gameFrame++
-    requestAnimationFrame(animate)
+    requestAnimationFrame(animate) // улучшенная версия setTime для анимаций и меньшей нагрузки
 }
 animate()
